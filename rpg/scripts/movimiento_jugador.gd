c6b6d28b574d22c0870408da8417531630bb5a2d
alumno26.melgarejo.juan@ipm.edu.ar <alumno26.melgarejo.juan@ipm.edu.ar>
2026-09-14 17:44:54 -0300

    Primeros assets y pruebas del primer escenario

--- rpg/scripts/movimiento_jugador.gd
+++ rpg/scripts/movimiento_jugador.gd
@@ -1,11 +1,43 @@
 extends Node
 
+var character_body: CharacterBody2D
+var animated_sprite: AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
+# Velocidad y movimiento
+var velocity: Vector2 = Vector2.ZERO
+var speed: float = 300.0
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	character_body = get_parent()
+	animated_sprite = character_body.get_node("AnimatedSprite2D")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	var input_velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		input_velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		input_velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		input_velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		input_velocity.y -= 1
+	
+	if input_velocity != Vector2.ZERO:
+		input_velocity = input_velocity.normalized()
+		velocity = input_velocity * speed
+		animated_sprite.play("Walk")
+		
+		# Girar el sprite según la dirección horizontal
+		if input_velocity.x < 0:
+			animated_sprite.flip_h = true
+		elif input_velocity.x > 0:
+			animated_sprite.flip_h = false
+	else:
+		velocity = Vector2.ZERO
+		animated_sprite.play("Idle")
+	
+	character_body.velocity = velocity
+	character_body.move_and_slide()
